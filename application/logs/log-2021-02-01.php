d7cb840e1de88581ebf1a89cbfc20b6139f716a2
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-01 22:15:44 +0530

    update

---
+++ application/logs/log-2021-02-01.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-02-01 15:34:46 --> Severity: Warning --> call_user_func_array() expects parameter 1 to be a valid callback, class 'Error' does not have a method 'index' C:\xampp\htdocs\Test\Smartbills-using-CIAS\system\core\CodeIgniter.php 532
+ERROR - 2021-02-01 15:34:58 --> Severity: Warning --> call_user_func_array() expects parameter 1 to be a valid callback, class 'Error' does not have a method 'index' C:\xampp\htdocs\Test\Smartbills-using-CIAS\system\core\CodeIgniter.php 532
+ERROR - 2021-02-01 17:10:18 --> Severity: Warning --> call_user_func_array() expects parameter 1 to be a valid callback, class 'Error' does not have a method 'index' C:\xampp\htdocs\Test\Smartbills-using-CIAS\system\core\CodeIgniter.php 532
